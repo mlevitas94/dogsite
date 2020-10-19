9b5005a99bb5a8a2e98468ec4a73ec9f46c9fc86
mlevitas94 <levitasmc@gmail.com>
2020-10-19 15:11:41 -0400

    dogsite created and fontawesome installed

---
+++ src/components/Nav.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="navCont">
+      <font-awesome-icon icon="bars"/>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
+/* eslint-disable */
 import Vue from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+ 
+library.add(faBars)
+ 
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false;
 
